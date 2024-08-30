66005618d58958e160851690d1e375e1440a72ea
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-08-30 22:42:17 +0300

    add: adds new files

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -4,10 +4,10 @@ export default function TermsOfUsePage() {
   return (
     <>
       <main className="mx-auto max-w-3xl p-5 pb-10 text-lg">
-        <h1>Terms of Use</h1>
+        <h1>Terms and Conditions</h1>
         
         <h2>1. Introduction</h2>
-        <p>By accessing or using our Service, you agree to be bound by these Terms of Use.</p>
+        <p>By accessing or using our Service, you agree to be bound by these Terms and Conditions.</p>
 
         <h2>2. Definitions</h2>
         <ul>
@@ -35,12 +35,12 @@ export default function TermsOfUsePage() {
 
         <h2>5. Intellectual Property Rights</h2>
         <p>
-          The Service and its original content, features and functionality are and will remain the exclusive property of Frases and its licensors.
+          The Service and its original content, features, and functionality are and will remain the exclusive property of Frases and its licensors.
         </p>
 
         <h2>6. User Conduct</h2>
         <p>
-          You agree not to use the Service for any unlawful purpose or in a way prohibited by these Terms of Use.
+          You agree not to use the Service for any unlawful purpose or in a way prohibited by these Terms and Conditions.
         </p>
 
         <h2>7. Termination</h2>
@@ -58,13 +58,13 @@ export default function TermsOfUsePage() {
           These Terms shall be governed and construed in accordance with the laws of the country where the Company is based, without regard to its conflict of law provisions.
         </p>
 
-        <h2>10. Changes to Terms of Use</h2>
+        <h2>10. Changes to Terms and Conditions</h2>
         <p>
-          We reserve the right, at Our sole discretion, to modify or replace these Terms at any time. If a revision is material, we will try to provide at least 30 days' notice prior to any new terms taking effect.
+          We reserve the right, at Our sole discretion, to modify or replace these Terms and Conditions at any time. If a revision is material, we will try to provide at least 30 days' notice prior to any new terms taking effect.
         </p>
 
         <h2>11. Contact Us</h2>
-        <p>If you have any questions about these Terms, please contact us:</p>
+        <p>If you have any questions about these Terms and Conditions, please contact us:</p>
         <ul>
           <li>
             By email:{" "}
